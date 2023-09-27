c9654e966cac1a331053cb4ae5aca71c5a0b472b
Harish251 <harishb1@kpmg.com>
2023-09-27 08:24:44 +0530

    Template

--- iocl/src/main/java/in/kpmg/iocl/controller/TestController.java
+++ iocl/src/main/java/in/kpmg/iocl/controller/TestController.java
@@ -14,6 +14,6 @@ public class TestController {
     @PostMapping("/fetch-details-with-txn")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ApiResponse2 fetchdetails(@RequestBody PaymentDto dto) {
-        return service.fetchAPI(dto);
+        return service.fetchAPI_YV_TPT_CUSTRTD(dto);
     }
 }
--- iocl/src/main/java/in/kpmg/iocl/service/TestService.java
+++ iocl/src/main/java/in/kpmg/iocl/service/TestService.java
@@ -17,7 +17,7 @@ import java.util.Base64;
 
 @Service
 public class TestService {
-    public ApiResponse2 fetchAPI(PaymentDto dto) {
+    public ApiResponse2 fetchAPI_YV_TPT_CUSTRTD(PaymentDto dto) {
         try {
             String url = "https://coisebizuat.ds.indianoil.in:7000/uat/RESTAdapter/RFC/MKHO/YV_TPT_CUSTRTD";
             String result="";
@@ -34,7 +34,7 @@ public class TestService {
             HTTPConnection.setRequestProperty("Authorization", "Basic " + hash);
 
             String jsonInputString = "{\n" +
-                    "\"VAL_DATE\": +"2022-09-09"+"\n" +
+                    "\"VAL_DATE\": 2022-09-09"+"\n" +
                     "\"I_BURKS\":" +""+ "\n" +
                     "\"I_WERKS\":" + "1140,31HS," + "\n" +
                     "}";
@@ -49,8 +49,9 @@ public class TestService {
                 response.append(inputLine);
             }
             result = response.toString();
-            ObjectMapper mapper = new ObjectMapper();
-            UidVault uidVault = mapper.readValue(result, UidVault.class);
+            return new ApiResponse2<>(true,"Success",result,200);
+//            ObjectMapper mapper = new ObjectMapper();
+//            UidVault uidVault = mapper.readValue(result, UidVault.class);
         } catch (ProtocolException e) {
             throw new RuntimeException(e);
         } catch (MalformedURLException e) {
@@ -61,17 +62,91 @@ public class TestService {
     }
     private String base64encoder (String str){
         return Base64.getEncoder().encodeToString(str.getBytes());
-        String url1 = "https://coisebizuat.ds.indianoil.in:7000/uat/RESTAdapter/RFC/MKHO/YV_LU_PCK_RATE";
-        String jsonInputString1 = "{\n" +
-                "\"VAL_DATE\": +"2022-09-09"+"\n" +
-                "\"I_BURKS\":" +""+ "\n" +
-                "\"I_WERKS\":" + "1140,31HS," + "\n" +
-                "}";
-        String url2 ="https://coisebizuat.ds.indianoil.in:6900/uat/RESTAdapter/RFC/YM_PO_DET_RFC_HO_LUBES";
-        String jsonInputString2 = "{\n" +
-                "\"CREATED_ON\": +"2022-09-09"+"\n" +
-                "\"MATERIAL_TYPE\":" +""+ "\n" +
-                "\"WERKS\":" + "1140,31HS," + "\n" +
-                "}";
+    }
+    public ApiResponse2 fetchAPI_YM_PO_DET_RFC_HO_LUBES(PaymentDto dto) {
+        try {
+            String url = "https://coisebizuat.ds.indianoil.in:6900/uat/RESTAdapter/RFC/YM_PO_DET_RFC_HO_LUBES";
+            String result="";
+
+
+
+            URL obj = new URL(url);
+            HttpURLConnection HTTPConnection = (HttpURLConnection) obj.openConnection();
+
+            HTTPConnection.setRequestMethod("POST");
+            HTTPConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            HTTPConnection.setDoOutput(true);
+            String hash = base64encoder("b2buser:iocl1234");
+            HTTPConnection.setRequestProperty("Authorization", "Basic " + hash);
+
+            String jsonInputString = "{\n" +
+                    "\"CREATED_ON\": 2022-09-09"+"\n" +
+                    "\"MATERIAL_TYPE\":" +""+ "\n" +
+                    "\"WERKS\":" + "1140,31HS," + "\n" +
+                    "}";
+            DataOutputStream writeRequest = new DataOutputStream(HTTPConnection.getOutputStream());
+            writeRequest.writeBytes(jsonInputString);
+            writeRequest.flush();
+            writeRequest.close();
+            BufferedReader in = new BufferedReader(new InputStreamReader(HTTPConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            result = response.toString();
+            return new ApiResponse2<>(true,"Success",result,200);
+//            ObjectMapper mapper = new ObjectMapper();
+//            UidVault uidVault = mapper.readValue(result, UidVault.class);
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public ApiResponse2 fetchAPI_YV_LU_PCK_RATE(PaymentDto dto) {
+        try {
+            String url = "https://coisebizuat.ds.indianoil.in:7000/uat/RESTAdapter/RFC/MKHO/YV_LU_PCK_RATE";
+            String result="";
+
+
+
+            URL obj = new URL(url);
+            HttpURLConnection HTTPConnection = (HttpURLConnection) obj.openConnection();
+
+            HTTPConnection.setRequestMethod("POST");
+            HTTPConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            HTTPConnection.setDoOutput(true);
+            String hash = base64encoder("b2buser:iocl1234");
+            HTTPConnection.setRequestProperty("Authorization", "Basic " + hash);
+
+            String jsonInputString = "{\n" +
+                    "\"VAL_DATE\": 2022-09-09"+"\n" +
+                    "\"I_BURKS\":" +""+ "\n" +
+                    "\"I_WERKS\":" + "1140,31HS," + "\n" +
+                    "}";
+            DataOutputStream writeRequest = new DataOutputStream(HTTPConnection.getOutputStream());
+            writeRequest.writeBytes(jsonInputString);
+            writeRequest.flush();
+            writeRequest.close();
+            BufferedReader in = new BufferedReader(new InputStreamReader(HTTPConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            result = response.toString();
+            return new ApiResponse2<>(true,"Success",result,200);
+//            ObjectMapper mapper = new ObjectMapper();
+//            UidVault uidVault = mapper.readValue(result, UidVault.class);
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
