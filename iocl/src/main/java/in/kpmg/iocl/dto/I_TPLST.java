9e29215815d240a6f40280086dd089c8f81a2af0
harishb2510 <137489558+harishb2510@users.noreply.github.com>
2023-10-30 14:47:29 +0530

    Updated error message

--- iocl/src/main/java/in/kpmg/iocl/dto/I_TPLST.java
+++ iocl/src/main/java/in/kpmg/iocl/dto/I_TPLST.java
@@ -3,11 +3,12 @@ package in.kpmg.iocl.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class I_TPLST {
     @JsonProperty("item")
-    private List<I_TPLST_Item> item;
+    private I_TPLST_Item item;
 
 }
