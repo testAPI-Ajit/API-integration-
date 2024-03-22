7578e8a6eff3194cec0e3a5dd96423ea6161ef2a
harishb2510 <harish25102000@gmail.com>
2024-03-22 21:00:29 +0530

    division_code_change

--- iocl/src/main/java/in/kpmg/iocl/models/MaterialMasterEmvkeModel.java
+++ iocl/src/main/java/in/kpmg/iocl/models/MaterialMasterEmvkeModel.java
@@ -25,15 +25,26 @@ public class MaterialMasterEmvkeModel {
 
     @Column(name = "division_code")
     private String division_code;
+    @Column(name = "matnr")
     private String MATNR;
+    @Column(name = "vkorg")
     private String VKORG;
+    @Column(name = "vtweg")
     private String VTWEG;
+    @Column(name = "kondm")
     private String KONDM;
+    @Column(name = "kondmt")
     private String KONDMT;
+    @Column(name = "ktgrm")
     private String KTGRM;
+    @Column(name = "ktgrmt")
     private String KTGRMT;
+    @Column(name = "mvgr1")
     private String MVGR1;
+    @Column(name = "mvgr2")
     private String MVGR2;
+    @Column(name = "mvgr3")
     private String MVGR3;
+    @Column(name = "dwerk")
     private String DWERK;
 }
--- iocl/src/main/java/in/kpmg/iocl/repository/WerksRepo.java
+++ iocl/src/main/java/in/kpmg/iocl/repository/WerksRepo.java
@@ -2,6 +2,8 @@ package in.kpmg.iocl.repository;
 
 import in.kpmg.iocl.models.WerksModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WerksRepo extends JpaRepository<WerksModel,Integer> {
 }
